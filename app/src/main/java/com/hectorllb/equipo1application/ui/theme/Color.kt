ea7690c64230e98baef00e5afcf9106ad0f1c5fd
alvarorodrigo-tajo <alvaro.rodrigocantalejo@riberadeltajo.es>
2026-03-24 21:08:20 +0100

    fix: inicio screen hecha

--- app/src/main/java/com/hectorllb/equipo1application/ui/theme/Color.kt
+++ app/src/main/java/com/hectorllb/equipo1application/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val AzulOscuro = Color(0xFF3D4A7A)   // botón Login y AppBar
+val MoradoClaro = Color(0xFF7B6FA0)  // botón Miuguago
+val FondoGris   = Color(0xFFF0F0F5)  // fondo de pantalla
